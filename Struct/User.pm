7e70c9eb4d8c3aec03e1846fe27387dd1a22d0b7
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-12-21 19:12:19 +0100

    Add doc for User. And examples for it

--- Struct/User.pm
+++ Struct/User.pm
@@ -38,3 +38,144 @@ sub struct2obj {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Toolforge::MixNMatch::Struct::User - Mix'n'match user structure serialization.
+
+=head1 SYNOPSIS
+
+ use Toolforge::MixNMatch::Struct::User qw(obj2struct struct2obj);
+
+ my $struct_hr = obj2struct($obj);
+ my $obj = struct2obj($struct_hr);
+
+=head1 DESCRIPTION
+
+This conversion is between object defined in Toolforge::MixNMatch::Object::User and structure
+serialized via JSON to Mix'n'match application.
+
+=head1 SUBROUTINES
+
+=head2 C<obj2struct>
+
+ my $struct_hr = obj2struct($obj);
+
+Convert Toolforge::MixNMatch::Object::Catalog instance to structure.
+
+Returns reference to hash with structure.
+
+=head2 C<struct2obj>
+
+ my $obj = struct2obj($struct_hr);
+
+Convert structure of time to object.
+
+Returns Toolforge::MixNMatch::Object::Catalog instance.
+
+=head1 EXAMPLE1
+
+ use strict;
+ use warnings;
+
+ use Data::Printer;
+ use Toolforge::MixNMatch::Object::User;
+ use Toolforge::MixNMatch::Struct::User qw(obj2struct);
+
+ # Object.
+ my $obj = Toolforge::MixNMatch::Object::User->new(
+         'count' => 6,
+         'uid' => 1,
+         'username' => 'Skim',
+ );
+
+ # Get structure.
+ my $struct_hr = obj2struct($obj);
+
+ # Dump to output.
+ p $struct_hr;
+
+ # Output:
+ # \ {
+ #     cnt        6,
+ #     uid        1,
+ #     username   "Skim"
+ # }
+
+=head1 EXAMPLE2
+
+ use strict;
+ use warnings;
+
+ use Toolforge::MixNMatch::Struct::User qw(struct2obj);
+
+ # Time structure.
+ my $struct_hr = {
+        'cnt' => 6,
+        'uid' => 1,
+        'username' => 'Skim',
+ };
+
+ # Get object.
+ my $obj = struct2obj($struct_hr);
+
+ # Get count.
+ my $count = $obj->count;
+
+ # Get user UID.
+ my $uid = $obj->uid;
+
+ # Get user name.
+ my $username = $obj->username;
+
+ # Print out.
+ print "Count: $count\n";
+ print "User UID: $uid\n";
+ print "User name: $username\n";
+
+ # Output:
+ # Count: 6
+ # User UID: 1
+ # User name: Skim
+
+=head1 DEPENDENCIES
+
+L<Exporter>,
+L<Readonly>,
+L<Toolforge::MixNMatch::Struct::User>.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Toolforge::MixNMatch::Struct>
+
+Toolforge Mix'n'match tool structures.
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/Toolforge-MixNMatch-Struct>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© Michal Josef Špaček 2020
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.04
+
+=cut
